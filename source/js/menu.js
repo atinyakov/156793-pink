1f74872bd7f3a7bd4078a5e53c9860bf6ae206ec
atinyakov <akoff16@gmail.com>
2018-05-24 18:09:48 +0300

    m9-task2

--- source/js/menu.js
+++ source/js/menu.js
@@ -5,6 +5,9 @@ var nav = document.querySelector('.main-nav');
 var navButton = document.querySelector('.main-nav__button');
 var mobileMenu = document.querySelector('.main-nav__sub-menu');
 
+nav.classList.remove('main-nav--opened');
+nav.classList.add('main-nav--closed');
+
 navButton.addEventListener('click', function () {
   if (nav.classList.contains('main-nav--closed')) {
     nav.classList.remove('main-nav--closed');
