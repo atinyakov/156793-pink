f286eb0528541eaa514b052998f2429d365dd1b9
atinyakov <akoff16@gmail.com>
2018-05-28 00:38:24 +0300

    final-fixed

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,21 +1,57 @@
 'use strict'
 
-var nav = document.querySelector('.main-nav');
+var DESKTOP_WIDTH = 940;
 var navButton = document.querySelector('.main-nav__button');
-var mobileMenu = document.querySelector('.main-nav__sub-menu');
+var mobileMenuHeader = document.querySelector('.main-nav__menu');
+var header = document.querySelector('.header');
+var mobileNav = document.querySelectorAll('.main-nav__item');
 
-nav.classList.remove('main-nav--opened');
-nav.classList.add('main-nav--closed');
-mobileMenu.classList.add('main-nav__sub-menu--hidden');
+console.log(header)
 
-navButton.addEventListener('click', function () {
-  if (nav.classList.contains('main-nav--closed')) {
-    nav.classList.remove('main-nav--closed');
-    nav.classList.add('main-nav--opened');
-    mobileMenu.classList.remove('main-nav__sub-menu--hidden');
-  } else {
-    nav.classList.add('main-nav--closed');
-    nav.classList.remove('main-nav--opened');
-    mobileMenu.classList.add('main-nav__sub-menu--hidden');
+var enableHeader = function () {
+  if (header.classList.contains('header--opened')) {
+      header.classList.remove('header--opened');
+  } else if (header.classList.contains('header-secondary--opened')) {
+    header.classList.remove('header-secondary--opened');
   }
-});
+}
+
+var toggleSecondaryHeader = function () {
+  if (header.classList.contains('header-secondary')) {
+      header.classList.toggle('header-secondary--opened');
+    }
+}
+
+if (window.innerWidth < DESKTOP_WIDTH) {
+
+  enableHeader();
+  mobileMenuHeader.classList.add('main-nav__menu--closed');
+
+  mobileNav.forEach(function(element) {
+    element.classList.add('main-nav__item--hidden');
+  });
+
+  navButton.addEventListener('click', function () {
+    if (mobileMenuHeader.classList.contains('main-nav__menu--closed')) {
+      mobileMenuHeader.classList.remove('main-nav__menu--closed');
+      mobileMenuHeader.classList.add('main-nav__menu--opened');
+
+      mobileNav.forEach(function(element) {
+        element.classList.remove('main-nav__item--hidden');
+      });
+
+      header.classList.add('header--opened');
+      toggleSecondaryHeader();
+    } else {
+      mobileMenuHeader.classList.remove('main-nav__menu--opened');
+      mobileMenuHeader.classList.add('main-nav__menu--closed');
+
+      mobileNav.forEach(function(element) {
+        element.classList.add('main-nav__item--hidden');
+      });
+
+      header.classList.remove('header--opened');
+      toggleSecondaryHeader();
+    }
+  });
+}
